304fc5af84b2cd006e21c9c3739544d27048d37f
ddabbi <suminkk04@gmail.com>
2024-04-20 12:56:35 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
-import {Container,Nav, Navbar,Button} from 'react-bootstrap';
+import {Container,Nav, Navbar,Button,Row,Col} from 'react-bootstrap';
 import data from './data';
+import { useState } from 'react';
 // 자바스크립트 파일은 확장자명생략
-import {num1,num2,num3} from './data'
+//import {num1,num2,num3} from './data'
+
+//이미지 사용하려면 import
+//import main from './img/banner.jpg';
 
 function App() {
   //css 제공 사이트: bootstrap
   //npm install react-bootstrap bootstrap
+  let [items, setItems] = useState(data);
+  let [photo, setPhoto] = useState(['./img/1.png','./img/1.png','./img/1.png']);
+
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">{data}</Navbar.Brand>
+          <Navbar.Brand href="#home"></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">{num1}</Nav.Link>
-            <Nav.Link href="#features">{num2}</Nav.Link>
-            <Nav.Link href="#pricing">{num3}</Nav.Link>
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">상세페이지</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
+
+  
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <ItemCol data={data[0]} img={'./img/1.png'}/>
+          <ItemCol data={data[1]} img={'./img/1.png'}/>
+          <ItemCol data={data[2]} img={'./img/1.png'}/>
+        </Row>
+      </Container>
+
+
         <Button variant="primary" size="lg">
           Large button
         </Button>{' '}
@@ -30,3 +48,17 @@ function App() {
 }
 
 export default App;
+
+//컴포넌트 맨 앞글자는 대문자
+//return에 html(jsx) 코드
+function ItemCol(props){
+  return(
+    <>
+      <Col>
+          <img src={'./img/1.png'} width={'200px'} height={'200px'}/>
+          <h4>{props.data.title}</h4>
+          <p>{props.data.price}원</p>
+      </Col>
+    </>
+  )
+}
--- src/data.js
+++ src/data.js
@@ -1,4 +1,23 @@
-let data = 10;
+let data = [
+  {
+    id: 0,
+    title: '모니터',
+    content:'',
+    price: 4000
+  },
+  {
+    id: 1,
+    title: '마우스',
+    content:'',
+    price: 4000
+  },
+  {
+    id: 2,
+    title: '키보드',
+    content:'',
+    price:4000
+  }
+];
 let num1 =1;
 let num2 =1;
 let num3 =1;
